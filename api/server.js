8d196a3afbf443a5c4d46bf9ef0148e0b85c0d9f
mrn720 <matt.ryan.norris@gmail.com>
2021-12-21 15:57:05 -0600

    MVP

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
-
 const server = express()
+const CarRouter = require('./cars/cars-router')
+
+server.use(express.json())
+server.use('/api/cars', CarRouter)
 
 // DO YOUR MAGIC
 
